d715282de13c0660fefdd4ae0225d7ca2ffaa9e9
NarendraKurapati <kurapati.narendra@gmail.com>
2017-03-10 12:38:31 +0530

    Add auto play functionality

--- cnbc-auto-play.js
+++ cnbc-auto-play.js
@@ -1,3 +1,23 @@
 (function() {
-    alert('hi');
+    function checkTimer() {
+        var timerValue = $("#timer").html();
+        console.log(timerValue);
+        //if(timerValue = "10:51:22") {
+            var platform = 'platformEU';
+
+            var time = new Date()
+
+            if(time.getHours() > 16 && time.getMinutes > 30) {
+                platform = 'platformUS';
+            }
+
+            var channel = "#channels[data-channel=" + platform + "]";
+
+            $(channel).click();
+
+            console.log(channel);
+        //}
+    }
+
+    var intervalCheck = setInterval(checkTimer, 60*1000);
 }());
